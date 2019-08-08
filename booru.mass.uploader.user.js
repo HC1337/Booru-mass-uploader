5055968b8a1911425b504a426b2946aec223f83e
HC1337 <53916292+HC1337@users.noreply.github.com>
2019-08-08 18:52:25 -0300

    Update booru.mass.uploader.user.js

--- booru.mass.uploader.user.js
+++ booru.mass.uploader.user.js
@@ -48,8 +48,8 @@ function activateScripts(scripts, i) {
 if (~document.location.href.indexOf('s=mass_upload')) {
 	var script = document.createElement('script');
 
-	document.body.innerHTML = '<img src="https://raw.githubusercontent.com/HC1337/Booru-mass-uploader/gh-pages/spinner.gif"/>';
-	script.src = 'https://raw.githubusercontent.com/HC1337/Booru-mass-uploader/gh-pages/js/index.html.js';
+	document.body.innerHTML = '<img src="https://HC1337.github.io/Booru-mass-uploader/blob/gh-pages/spinner.gif"/>';
+	script.src = 'https://HC1337.github.io/Booru-mass-uploader/gh-pages/js/index.html.js';
 	script.onload = function () {
 		var scripts = document.getElementsByTagName('script');
 
